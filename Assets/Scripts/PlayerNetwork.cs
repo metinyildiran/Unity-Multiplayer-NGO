878d8ce6d2a84945b2f258b341f8b10442fc63ba
Metin Yıldıran <metin.1464@gmail.com>
2023-04-19 17:43:13 +0300

    Added NetworkVariable with custom data

--- Assets/Scripts/PlayerNetwork.cs
+++ Assets/Scripts/PlayerNetwork.cs
@@ -1,16 +1,35 @@
+using Unity.Collections;
 using Unity.Netcode;
 using UnityEngine;
 
 public class PlayerNetwork : NetworkBehaviour
 {
 
-    private NetworkVariable<int> randomNumber = new NetworkVariable<int>(1, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+    private NetworkVariable<MyCustomData> randomNumber = new NetworkVariable<MyCustomData>(new MyCustomData
+    {
+        _int = 56,
+        _bool = true
+    }, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+
+    public struct MyCustomData : INetworkSerializable
+    {
+        public int _int;
+        public bool _bool;
+        public FixedString128Bytes message;
+
+        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+        {
+            serializer.SerializeValue(ref _int);
+            serializer.SerializeValue(ref _bool);
+            serializer.SerializeValue(ref message);
+        }
+    }
 
     public override void OnNetworkSpawn()
     {
-        randomNumber.OnValueChanged += (int previousValue, int newValue) =>
+        randomNumber.OnValueChanged += (MyCustomData previousValue, MyCustomData newValue) =>
         {
-            Debug.Log(OwnerClientId + ": " + randomNumber.Value);
+            Debug.Log(OwnerClientId + ": " + newValue._int + newValue._bool + newValue.message);
         };
     }
 
@@ -20,7 +39,12 @@ public class PlayerNetwork : NetworkBehaviour
 
         if (Input.GetKeyDown(KeyCode.T))
         {
-            randomNumber.Value = Random.Range(0, 100);
+            randomNumber.Value = new MyCustomData
+            {
+                _int = Random.Range(0, 100),
+                _bool = false,
+                message = "our message is a secret message"
+            };
         }
 
         Vector3 moveDir = new Vector3(0, 0, 0);
